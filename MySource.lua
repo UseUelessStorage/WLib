8be6d0297761aed199c77656933d7054535153da
UseUelessStorage <fmail6555@gmail.com>
2025-02-14 19:31:56 +0100

    Update MySource.lua

--- MySource.lua
+++ MySource.lua
@@ -355,8 +355,8 @@ local function MakeDrag(Instance)
 		local function Update(Input)
 			local delta = Input.Position - DragStart
 			local Position = UDim2.new(StartPos.X.Scale, StartPos.X.Offset + delta.X / UIScale, StartPos.Y.Scale, StartPos.Y.Offset + delta.Y / UIScale)
-			 -- Instance.Position = Position
-			CreateTween({Instance, "Position", Position, 0.3})
+			-- Instance.Position = Position
+			CreateTween({Instance, "Position", Position, 0.35})
 		end
 		
 		Instance.MouseButton1Down:Connect(function()
